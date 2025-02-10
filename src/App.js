e7ac0c2c66afe9c03f77a5c6a717fec2fea1524f
Kavya Naik <kavyanaik2112@gmail.com>
2025-02-10 21:17:23 +0530

    netflix-gpt

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-3xl font-bold underline text-orange-600'>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
